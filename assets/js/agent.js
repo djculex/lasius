172cd5347a33d2b9ecd83ec95d18fbcb6c0f6553
Michael Albertsen <culex@culex.dk>
2021-02-09 14:10:53 +0100

    addtions and fixes
    Fixed interval being too short by ms * 1000. Added responsive aside check to allow different themes than default

--- assets/js/agent.js
+++ assets/js/agent.js
@@ -3,7 +3,7 @@ jQuery( document ).ready(function() {
 	jQuery( window ).on( "load", function() {
 		//console.log("window loaded");
 		//alert(Lasius_refurl);
-
+		
 			// start interval after 10000 ms			
 			var interValStart_onlineusers = setInterval(SystemBlock_checkOnlineUsers, 5000); 
 			var interValStart_topposters = setInterval(SystemBlock_checkTopPosters, 5000); 
@@ -18,20 +18,20 @@ jQuery( document ).ready(function() {
 			  // while interalstart not set do nothing
 			  //console.log("Online should be closed = " + Lasius_reviveonlineusersblock);
 				if (!interValStart_onlineusers) {
-					//console.log("Online = " + Lasius_reviveonlineusersblock);
-					interValStart_onlineusers = (Lasius_reviveonlineusersblock > 0) ? setInterval(SystemBlock_checkOnlineUsers, Lasius_reviveonlineusersblock):interValStart_onlineusers = undefined; 
+					console.log("Online = " + Lasius_reviveonlineusersblock);
+					interValStart_onlineusers = (Lasius_reviveonlineusersblock > 0) ? setInterval(SystemBlock_checkOnlineUsers, (Lasius_reviveonlineusersblock * 1000)):interValStart_onlineusers = undefined; 
 				}
 				
 				if (!interValStart_topposters) {
-					interValStart_topposters = (Lasius_revivetoppostersblock > 0) ? setInterval(SystemBlock_checkTopPosters, Lasius_revivetoppostersblock) : interValStart_topposters = undefined; 
+					interValStart_topposters = (Lasius_revivetoppostersblock > 0) ? setInterval(SystemBlock_checkTopPosters, (Lasius_revivetoppostersblock * 1000)) : interValStart_topposters = undefined; 
 				}
 				
 				if (!interValStart_newmembers) {
-					interValStart_newmembers = (Lasius_revivenewmembersblock > 0) ? setInterval(SystemBlock_checkNewMembers, Lasius_revivenewmembersblock) : interValStart_newmembers = undefined; 
+					interValStart_newmembers = (Lasius_revivenewmembersblock > 0) ? setInterval(SystemBlock_checkNewMembers, (Lasius_revivenewmembersblock * 1000)) : interValStart_newmembers = undefined; 
 				}
 				
 				if (!interValStart_checkrecentcomments) {
-					interValStart_checkrecentcomments = (Lasius_reviverecentcommentsblock > 0) ? setInterval(SystemBlock_checkRecentComments, Lasius_reviverecentcommentsblock) : interValStart_checkrecentcomments = undefined; 
+					interValStart_checkrecentcomments = (Lasius_reviverecentcommentsblock > 0) ? setInterval(SystemBlock_checkRecentComments, (Lasius_reviverecentcommentsblock * 1000)) : interValStart_checkrecentcomments = undefined; 
 				}
 				
 			});
@@ -47,46 +47,58 @@ jQuery( document ).ready(function() {
 			// Check online users function called with interval.
 			
 			function SystemBlock_checkNewMembers() {
-				this.interValStart_newmembers = undefined; //reset interval
+				this.interValStart_newmembers = undefined; //reset interval				
 				if (Lasius_revivenewmembersblock_title > 0) {
 					jQuery('div:contains("' + Lasius_revivenewmembersblock_title + '")').next(".blockContent").load(Lasius_systemurl + "/agent.php?type=newmembers", function() {
 						//console.log('replace  new members block.');
 						//console.log(Lasius_systemurl + "/agent.php?type=newmembers");
 					}).fadeIn('slow');
+					
+					jQuery('aside h4.block-title:contains("' + Lasius_revivenewmembersblock_title + '")').parent("aside").load(Lasius_systemurl + "/agent.php?type=newmembers", function() {
+						jQuery(this).prepend('<h4 class="block-title">' + Lasius_revivenewmembersblock_title + '</h4>');
+							//console.log('replace  topposters block.');
+							//console.log(Lasius_systemurl + "/agent.php?type=topposters");
+					}).fadeIn('slow');					
 				}
-								
+				
 			}
 			
 			function SystemBlock_checkTopPosters() {
-				this.interValStart_topposters = undefined; //reset interval
+				this.interValStart_topposters = undefined; //reset interval			
 				if (Lasius_revivetoppostersblock > 0) {
 					jQuery('div:contains("' + Lasius_revivetoppostersblock_title + '")').next(".blockContent").load(Lasius_systemurl + "/agent.php?type=topposters", function() {
 						//console.log('replace  topposters block.');
 						//console.log(Lasius_systemurl + "/agent.php?type=topposters");
 					}).fadeIn('slow');
+					
+					jQuery('aside h4.block-title:contains("' + Lasius_revivetoppostersblock_title + '")').parent("aside").load(Lasius_systemurl + "/agent.php?type=topposters", function() {
+						jQuery(this).prepend('<h4 class="block-title">' + Lasius_revivetoppostersblock_title + '</h4>');
+							//console.log('replace  topposters block.');
+							//console.log(Lasius_systemurl + "/agent.php?type=topposters");
+					}).fadeIn('slow');
 				}
-								
+					
 			}
 
 			// function called with interval.
 			function SystemBlock_checkOnlineUsers() {
-					/*
-					jQuery(
-						'div:contains("'+Lasius_reviveonlineusers_searcharray[0]+'")' && 
-						'div:contains("'+Lasius_reviveonlineusers_searcharray[1]+'")' && 
-						'div:contains("'+Lasius_reviveonlineusers_searcharray[2]+'")').load(Lasius_systemurl + "/agent.php?type=onlinenow", function() {
-							console.log('replace online users block.');
-						}).fadeIn('slow');
-					*/
-					this.interValStart_onlineusers = undefined; //reset interval
+					// if interval is set do smarty include
+					this.interValStart_onlineusers = undefined; //reset interval	
 					if (Lasius_reviveonlineusersblock > 0) {
 						jQuery('div:contains("' + Lasius_reviveonlineusersblock_title + '")').next(".blockContent").load(Lasius_systemurl + "/agent.php?type=onlinenow&name="+Lasius_refurl, function() {
 							//console.log('replace online users block.');
 							//console.log(Lasius_systemurl + "/agent.php?type=onlinenow");
 						}).fadeIn('slow');
+						
+						
+						jQuery('.block-title:contains("' + Lasius_reviveonlineusersblock_title + '")').parent().load(Lasius_systemurl + "/agent.php?type=onlinenow&name="+Lasius_refurl, function() {
+							jQuery(this).prepend('<h4 class="block-title">' + Lasius_reviveonlineusersblock_title + '</h4>');
+							//console.log('replace online users block.');
+							//console.log(Lasius_systemurl + "/agent.php?type=onlinenow");
+						}).fadeIn('slow');
+						
 					}
-					// if interval is set do smarty include
-									
+								
 			}
 			
 			// Check recent comments.
@@ -96,6 +108,12 @@ jQuery( document ).ready(function() {
 						jQuery('div:contains("' + Lasius_reviverecentcommentsblock_title + '")').next(".blockContent").load(Lasius_systemurl + "/agent.php?type=recentcomments", function() {
 						//console.log('replace  topposters block.');
 						//console.log(Lasius_systemurl + "/agent.php?type=topposters");
+						}).fadeIn('slow');
+						
+						jQuery('aside h4.block-title:contains("' + Lasius_reviverecentcommentsblock_title + '")').parent("aside").load(Lasius_systemurl + "/agent.php?type=recentcomments", function() {
+							jQuery(this).prepend('<h4 class="block-title">' + Lasius_reviverecentcommentsblock_title + '</h4>');
+								//console.log('replace  topposters block.');
+								//console.log(Lasius_systemurl + "/agent.php?type=topposters");
 						}).fadeIn('slow');
 					}
 								
