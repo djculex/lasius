ec534b4bef462b68003736f2846a3cb04a6911e0
Michael Albertsen <culex@culex.dk>
2021-04-04 12:57:49 +0200

    Update lasius.js
    problem with dialog temporarely fixed by including jquery.ui in dynamic content.

--- assets/js/lasius.js
+++ assets/js/lasius.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function () {
+	//debugger;
 	/*
 	 * Overtake the pop-up messages and use jqueryUi dialog instead
 	 * return false
@@ -10,13 +11,15 @@ jQuery(document).ready(function () {
 			// ---- start ----
 			if (this.href.indexOf('showpopups&type=online') != -1) {
 					event.preventDefault();
-					jQuery( "#lasius_ou" ).dialog( "destroy" ); // Close any existing dialogues before opening new		
+					//jQuery( "#lasius_ou" ).dialog( "destroy" ); // Close any existing dialogues before opening new		
 					// remove part of user id links that opens pop-up
 					var url = this.href;
 					var newurl = url.replace("javascript:openWithSelfMain('", "").replace("','Online',420,350);","");
 				// initiate jquery UI dialog
 				jQuery('<div id="lasius_ou">').dialog({			
 					modal: false,
+					closeText : '',		
+					height:'auto', width:'auto',
 					open: function ()
 					{
 						jQuery("#lasius_ou").hide(); // hide html until load and manipulation is done
@@ -49,8 +52,31 @@ jQuery(document).ready(function () {
 					return false;
 			}
 			// ---- end ----
-			
+		});
+	} // ----- end of popups -----
+
+	if (Lasius_usepopups > 0) {
+		/*
+		 * if clicked on show more display extended info box
+		 *
+		 *
+		*/
+		
+		$(document).on('click', 'button#lasius-online-users-selector-new' , function(event) {
+				$('#lasius-hidden-users').dialog({
+					closeText : '',		
+					height:'auto', width:'auto',					
+					modal: false,
+					open: function ()
+					{
+						$("#lasius-hidden-users").show(); // hide html until load and manipulation is done					
+					},
+					title: Lasius_reviveonlineusersblock_title // set dialog title			
+				});
+					return false;			
 		});
 	}
+	
+
 
 });
