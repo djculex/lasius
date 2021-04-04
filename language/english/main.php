3494c821960035f75d7f9a4dccea2bebce14ba7b
Michael Albertsen <culex@culex.dk>
2021-04-04 12:58:24 +0200

    Update main.php

--- language/english/main.php
+++ language/english/main.php
@@ -1,5 +1,8 @@
 <?php
 /* Lasius for Xoops 2.5x */
-
-
- 
+define('_LASIUS_BLOCK_LOCATIONFLAG', 'Flag');
+define('_LASIUS_BLOCK_LOCATIONCOUNTRYCITY', 'Country / city');
+define('_LASIUS_BLOCK_USERNAME', 'Username');
+define('_LASIUS_BLOCK_WHEREINPAGE', 'Where ?');
+define('_LASIUS_BLOCK_AVATAR', 'Image');
+define('_LASIUS_BLOCK_ANONYMOUS', 'Anonymous');
